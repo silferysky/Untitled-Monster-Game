efb3de05060e5441ec0ebce90dabb9aab9f9cfb6
Terence <deathysayshi@gmail.com>
2021-02-16 17:26:50 +0800

    Basic Movement and Bounding box level

---
+++ Untitled Monster Game/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Vector3 m_playerLocation;
+	float MovementSpeed = 5.0f;
+	
+	Rigidbody2D rigidbody;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_playerLocation = new Vector3(0.0f, 0.0f, -5.0f);
+		rigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    Vector3 moveVec = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveVec.y += 1.0f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveVec.y -= 1.0f;
+        }
+        if (Input.GetKey(KeyCode.A))
+	    {
+            moveVec.x -= 1.0f;
+	    }
+	    if (Input.GetKey(KeyCode.D))
+	    {
+            moveVec.x += 1.0f;
+	    }
+		
+		rigidbody.AddForce(moveVec);
+		
+        //m_playerLocation += moveVec * MovementSpeed * Time.deltaTime;
+        //transform.position = m_playerLocation;
+    }
+}
