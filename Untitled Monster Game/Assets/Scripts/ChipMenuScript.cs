c368d542201894ec80c388f34c38d19f795d848f
Terence Chan <deathysayshi@gmail.com>
2021-02-23 01:23:25 +0800

    ChipScripts display and logic

--- Untitled Monster Game/Assets/Scripts/ChipMenuScript.cs
+++ Untitled Monster Game/Assets/Scripts/ChipMenuScript.cs
@@ -5,9 +5,10 @@ using Chip = ChipScript;
 
 public class ChipMenuScript : MonoBehaviour
 {
-	public List<Chip> AttachedChips;
-	public List<GameObject> DisplayedChips;
-	public List<GameObject> ChipDisplays;
+	public List<Chip> AttachedChips = new List<Chip>();
+	public List<GameObject> DisplayedChips = new List<GameObject>();
+	public List<GameObject> ChipTemplate = new List<GameObject>();
+    public GameObject Background;
 	public int MaxChipSize;
 	public int CurChipSize;
 	
@@ -15,6 +16,20 @@ public class ChipMenuScript : MonoBehaviour
     void Start()
     {
         CurChipSize = 0;
+        AttachedChips.Add(new Chip(0, 1, 1, 0, "First"));
+        AttachedChips.Add(new Chip(0, 1, 2, 1, "Second"));
+        AttachedChips.Add(new Chip(0, 1, 3, 2, "Third"));
+        AttachedChips.Add(new Chip(0, 1, 2, 0, "Fourth"));
+        AttachedChips.Add(new Chip(0, 1, 1, 0, "Fifth"));
+
+
+        foreach (Chip c in AttachedChips)
+        {
+            c.EvaluateChip();
+        }
+        
+        SortChips();
+        DisplayChips();
     }
 
     // Update is called once per frame
@@ -44,10 +59,88 @@ public class ChipMenuScript : MonoBehaviour
 			Destroy(go);
 		}
 		DisplayedChips.Clear();
-		
+
+        Vector3 curPos = transform.position + new Vector3(0.0f, -0.2f + Background.GetComponent<SpriteRenderer>().bounds.size.y / 2, -1.0f);
+
 		foreach (Chip c in AttachedChips)
 		{
-			//Instantiate()
-		}
+            //Instantiate
+            GameObject toInstantiate;
+            if (c.ChipSize == 1)
+            {
+                toInstantiate = ChipTemplate[0];
+            }
+            else if (c.ChipSize == 2)
+            {
+                toInstantiate = ChipTemplate[1];
+            }
+            else //if (c.ChipSize == 3)
+            {
+                toInstantiate = ChipTemplate[2];
+            }
+
+            switch (c.ChipType)
+            {
+                case 0:
+                    toInstantiate.GetComponent<SpriteRenderer>().color = new Color(0.55f, 0.21f, 0.25f);
+                    break;
+                case 1:
+                    toInstantiate.GetComponent<SpriteRenderer>().color = new Color(0.45f, 0.41f, 0.27f);
+                    break;
+                case 2:
+                    toInstantiate.GetComponent<SpriteRenderer>().color = new Color(0.5f, 0.55f, 0.47f);
+                    break;
+                default:
+                    toInstantiate.GetComponent<SpriteRenderer>().color = Color.white;
+                    break;
+            }
+
+            //This is to offset Chip Size
+            DisplayedChips.Add(Instantiate(toInstantiate, curPos + new Vector3(0.0f, toInstantiate.GetComponent<SpriteRenderer>().bounds.size.y / -2, 0.0f), Quaternion.identity));
+
+            curPos += new Vector3(0.0f, -0.1f - toInstantiate.GetComponent<SpriteRenderer>().bounds.size.y, 0.0f);
+        }
 	}
+
+    void SortChips()
+    {
+        AttachedChips.Sort();
+		AttachedChips.Reverse();
+        //List<Chip> TempHolder = new List<Chip>();
+        //List<Chip> TempChips = new List<Chip>();
+
+        //for (int i = 0; i < AttachedChips.Count; ++i)
+        //{
+        //    Chip c = new Chip();
+        //    c.CopyChip(AttachedChips[i]);
+        //    TempChips.Add(c);
+        //}
+
+        //Chip BestChip = new Chip();
+        //int ID = 0;
+        //int HighestPoints = 0;
+
+        ////Loop X number of chips times
+        //for (int i = 0; i < AttachedChips.Count; ++i)
+        //{
+        //    //Loop through to find best chip
+        //    for (int j = 0; j < TempChips.Count; ++j)
+        //    {
+        //        Chip c = new Chip();
+        //        c.CopyChip(TempChips[j]);
+        //        if (c.ChipPoints > HighestPoints)
+        //        {
+        //            HighestPoints = c.ChipPoints;
+        //            BestChip.CopyChip(c);
+        //            ID = j;
+        //        }
+        //    }
+        //    TempHolder.Add(BestChip);
+        //    TempChips.RemoveAt(ID);
+        //    BestChip = new Chip();
+        //}
+
+        //AttachedChips = TempHolder;
+    }
+
 }
--- Untitled Monster Game/Assets/Scripts/ChipScript.cs
+++ Untitled Monster Game/Assets/Scripts/ChipScript.cs
@@ -1,15 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChipScript : MonoBehaviour
+public class ChipScript : IComparable<ChipScript>
 {
 	public int ChipID;
 	public int ChipLevel;
 	public int ChipSize;
-	public int ChipType;
+	public int ChipType; //0 = Atk, 1 = Stat, 2 = UI
 	public string ChipName;
-	
+    public int ChipPoints; //Used for ChipMenu calculations
+
+    public ChipScript()
+    {
+        ChipID = 0;
+        ChipLevel = 0;
+        ChipSize = 0;
+        ChipType = 0;
+        ChipName = "DEFAULT";
+        ChipPoints = -1;
+    }
+
+    public ChipScript(int id, int level, int size, int type, string name)
+    {
+        ChipID = id;
+        ChipLevel = level;
+        ChipSize = size;
+        ChipType = type;
+        ChipName = name;
+
+        EvaluateChip();
+    }
+
+    public void CopyChip(ChipScript rhs)
+    {
+        ChipID = rhs.ChipID;
+        ChipLevel = rhs.ChipLevel;
+        ChipSize = rhs.ChipSize;
+        ChipType = rhs.ChipType;
+        ChipName = rhs.ChipName;
+        ChipPoints = rhs.ChipPoints;
+    }
+
+    public void EvaluateChip()
+    {
+        //ChipType > ChipSize > ChipLevel
+        ChipPoints = 100 * (3 - ChipType) + 10 * (ChipSize) + 1 * (ChipLevel);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +59,17 @@ public class ChipScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public int CompareTo(ChipScript rhs)
+    {
+        if (rhs == null)
+        {
+            return 1;
+        }
+        else
+        {
+            return this.ChipPoints.CompareTo(rhs.ChipPoints);
+        }
     }
 }
