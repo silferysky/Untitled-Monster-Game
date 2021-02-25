0be4f3b34af3627c4f641c8f987a9a398de4b30a
Mark Tan <ktan.mark@hotmail.com>
2021-02-25 22:02:49 +0800

    Really basic damage system

--- Untitled Monster Game/Assets/Scripts/DoDamageScript.cs
+++ Untitled Monster Game/Assets/Scripts/DoDamageScript.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DoDamageScript : MonoBehaviour
-{	
+{
+    public bool EnableRanged = false;
+    public int RangedDamage = 1;
+
+    public bool EnableMelee = false;
+    public int MeleeDamage = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +18,36 @@ public class DoDamageScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
 
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (tag == "AI")
+        {
+            if (other.tag == "Player")
+            {
+                HealthScript hs = other.transform.gameObject.GetComponent<HealthScript>();
+
+                if (hs)
+                    hs.TakeDamage(MeleeDamage);
+            }
+        }
+
+        if (tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                if (other.tag == "AI")
+                {
+                    HealthScript hs = other.transform.gameObject.GetComponent<HealthScript>();
+
+                    if (hs)
+                    {
+                        hs.TakeDamage(MeleeDamage);
+                    }
+                }
+            }
+        }
     }
 }
---
+++ Untitled Monster Game/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    public int HP = 10;
+    bool isAlive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isAlive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (!isAlive)
+            return;
+
+        HP -= damage;
+
+        // Hard limit minimal hp to 0
+        if (HP < 0)
+            HP = 0;
+
+        if (HP == 0)
+        {
+            isAlive = false;
+            print("YOU DIED!");
+        }
+    }
+}
