4e67348189635373cd329e0477a594858bc37c7a
Mark Tan <ktan.mark@hotmail.com>
2021-03-01 16:40:54 +0800

    Player melee/ranged selection indicator

---
+++ Untitled Monster Game/Assets/Scripts/UI Scripts/WeaponSelectUIScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponSelectUIScript : MonoBehaviour
+{
+    public GameObject Player;
+    DoDamageScript dmgScript;
+
+    public Image MeleeIcon;
+    public Image RangedIcon;
+
+    Vector3 inactiveScale = new Vector3(1.0f, 1.0f, 1.0f);
+    Vector3 activeScale = new Vector3(1.2f, 1.2f, 1.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dmgScript = Player.GetComponent<DoDamageScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dmgScript.EnableMelee)
+        {
+            MeleeIcon.transform.localScale = activeScale;
+            RangedIcon.transform.localScale = inactiveScale;
+        }
+
+        if (dmgScript.EnableRanged)
+        {
+            MeleeIcon.transform.localScale = inactiveScale;
+            RangedIcon.transform.localScale = activeScale;
+        }
+    }
+}
