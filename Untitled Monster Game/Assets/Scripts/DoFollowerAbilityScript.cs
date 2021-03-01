4fa9f021c7a7bd2efd357cc6402c27d0523c1459
Mark Tan <ktan.mark@hotmail.com>
2021-03-01 17:01:57 +0800

    Added generic follower do ability script
    Not sure how we gonna implement the follower abilties tbh

---
+++ Untitled Monster Game/Assets/Scripts/DoFollowerAbilityScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoFollowerAbilityScript : MonoBehaviour
+{
+    HealthScript myhealthscript;
+
+    public float AbilityCooldown = 1.0f;
+    public float AbilityCooldownTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myhealthscript = GetComponent<HealthScript>();
+        AbilityCooldownTimer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AbilityCooldownTimer > 0.0f)
+            AbilityCooldownTimer -= Time.deltaTime;
+
+        if (AbilityCooldownTimer < 0.0f)
+            AbilityCooldownTimer = 0.0f;
+
+        DoAbility();
+    }
+
+    void DoAbility()
+    {
+        if (AbilityCooldownTimer == 0.0f)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                // Do Ability
+
+
+                // Cooldown
+                AbilityCooldownTimer = AbilityCooldown;
+            }
+
+        }
+    }
+
+    public float GetCooldownAsFraction()
+    {
+        return (AbilityCooldownTimer / AbilityCooldown);
+    }
+
+    public float GetCooldownTimer()
+    {
+        return AbilityCooldownTimer;
+    }
+}
---
+++ Untitled Monster Game/Assets/Scripts/UI Scripts/FollowerAbilityCooldownScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FollowerAbilityCooldownScript : MonoBehaviour
+{
+    DoFollowerAbilityScript doAbilScript;
+    Image cooldown_fill;
+    Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cooldown_fill = GetComponent<Image>();
+
+        GameObject[] array = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject go in array)
+        {
+            if (go.GetComponent<DoFollowerAbilityScript>())
+            {
+                doAbilScript = go.GetComponent<DoFollowerAbilityScript>();
+                break;
+            }
+        }
+
+        foreach (Transform child in transform.parent)
+        {
+            if (child.tag == "TimerText")
+            {
+                timerText = child.GetComponent<Text>();
+                break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldown_fill.fillAmount = doAbilScript.GetCooldownAsFraction();
+
+        float cooldown_timer = doAbilScript.GetCooldownTimer();
+        if (cooldown_timer > 0)
+            timerText.text = string.Format("{0:0.#}", cooldown_timer);
+        else
+            timerText.text = "";
+    }
+}
