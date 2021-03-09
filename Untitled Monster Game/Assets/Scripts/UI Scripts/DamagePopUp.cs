5f6a99782c4a516b51afe32c3cd6ccca2db1fb9b
Mark Tan <ktan.mark@hotmail.com>
2021-03-09 15:56:05 +0800

    damage popup done

--- Untitled Monster Game/Assets/Scripts/UI Scripts/DamagePopUp.cs
+++ Untitled Monster Game/Assets/Scripts/UI Scripts/DamagePopUp.cs
@@ -7,18 +7,35 @@ public class DamagePopUp : MonoBehaviour
 {
     public Text TextObj;
     public Vector2 Force = new Vector2(0.0f, 2.0f);
+    public float Lifetime = 1.0f;
+    public float FadeDuration = 1.0f;
+
     int damage;
+    Color origColor;
+    float fadeTimer;
+    float lifeTimer;
 
     // Start is called before the first frame update
     void Start()
     {
         TextObj.text = damage.ToString();
         GetComponent<Rigidbody2D>().AddForce(Force, ForceMode2D.Impulse);
+        origColor = TextObj.color;
     }
 
     // Update is called once per frame
     void Update()
     {
+        lifeTimer += Time.deltaTime;
+        fadeTimer += Time.deltaTime;
+
+        if (lifeTimer >= Lifetime)
+        {
+            Destroy(this);
+            return;
+        }
+
+        TextObj.color = new Color(origColor.r, origColor.b, origColor.g, Mathf.Lerp(origColor.a, 0.0f, fadeTimer / FadeDuration));
     }
 
     public void SetParams(int _damage)
