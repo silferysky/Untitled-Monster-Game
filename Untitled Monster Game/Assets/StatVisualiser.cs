ec88c886dd50417d86ded5a9397f70b9be98b36f
Javier <javier_foo@hotmail.com>
2021-03-11 05:08:10 +0800

    wip

---
+++ Untitled Monster Game/Assets/StatVisualiser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatVisualiser : MonoBehaviour
+{
+    public GameObject[] bars;
+    public int value = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < value; ++i)
+        {
+            bars[i].SetActive(true);
+        }
+    }
+}
