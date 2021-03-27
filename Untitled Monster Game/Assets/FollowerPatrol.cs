552ab05d5383cd1adbcf0ba83ac7894dc71959e6
Mark Tan <ktan.mark@hotmail.com>
2021-03-27 15:30:45 +0800

    commented out some broken code from javier's stuff

--- Untitled Monster Game/Assets/FollowerPatrol.cs
+++ Untitled Monster Game/Assets/FollowerPatrol.cs
@@ -23,47 +23,47 @@ public class FollowerPatrol : StateMachineBehaviour
 
     }
 
-    void CheckAttackRange()
-    {
-        Collider2D[] other = Physics2D.OverlapCircleAll(AttackPos.position, AttackRange, PlayerMask);
+    //void CheckAttackRange()
+    //{
+    //    Collider2D[] other = Physics2D.OverlapCircleAll(AttackPos.position, AttackRange, PlayerMask);
 
-        if (other.Length > 0)
-        {
-            rigidbody.velocity = new Vector3(0.0f, 0.0f, 0.0f);
-            animator.SetFloat("Velocity", 0.0f);
+    //    if (other.Length > 0)
+    //    {
+    //        rigidbody.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+    //        animator.SetFloat("Velocity", 0.0f);
 
-            isAttacking = true;
-        }
-        else
-            isAttacking = false;
+    //        isAttacking = true;
+    //    }
+    //    else
+    //        isAttacking = false;
 
-        foreach (Collider2D otherCollider in other)
-        {
-            if (otherCollider.GetComponent<HealthScript>() != null &&
-                !otherCollider.GetComponent<HealthScript>().GetAlive())
-            {
-                isAttacking = false;
-            }
-        }
-    }
+    //    foreach (Collider2D otherCollider in other)
+    //    {
+    //        if (otherCollider.GetComponent<HealthScript>() != null &&
+    //            !otherCollider.GetComponent<HealthScript>().GetAlive())
+    //        {
+    //            isAttacking = false;
+    //        }
+    //    }
+    //}
 
-    private void FlipSprite(bool face_right_this_frame)
-    {
-        if (face_right_this_frame && !isFacingRight)
-        {
-            transform.localRotation = Quaternion.Euler(0, 0, 0);
-            isFacingRight = true;
+    //private void FlipSprite(bool face_right_this_frame)
+    //{
+    //    if (face_right_this_frame && !isFacingRight)
+    //    {
+    //        transform.localRotation = Quaternion.Euler(0, 0, 0);
+    //        isFacingRight = true;
 
-            if (canvas)
-                canvas.transform.localRotation = Quaternion.Euler(0, 0, 0);
-        }
-        else if (!face_right_this_frame && isFacingRight)
-        {
-            transform.localRotation = Quaternion.Euler(0, 180, 0);
-            isFacingRight = false;
+    //        if (canvas)
+    //            canvas.transform.localRotation = Quaternion.Euler(0, 0, 0);
+    //    }
+    //    else if (!face_right_this_frame && isFacingRight)
+    //    {
+    //        transform.localRotation = Quaternion.Euler(0, 180, 0);
+    //        isFacingRight = false;
 
-            if (canvas)
-                canvas.transform.localRotation = Quaternion.Euler(0, 180, 0);
-        }
-    }
+    //        if (canvas)
+    //            canvas.transform.localRotation = Quaternion.Euler(0, 180, 0);
+    //    }
+    //}
 }
